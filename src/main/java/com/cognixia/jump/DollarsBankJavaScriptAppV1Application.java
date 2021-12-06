750b9d11637c91dab3e55eabcb0877a4b4370108
zacksafadi <33300646+zacksafadi@users.noreply.github.com>
2021-12-06 10:29:01 -0500

    initial commit

---
+++ src/main/java/com/cognixia/jump/DollarsBankJavaScriptAppV1Application.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsBankJavaScriptAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsBankJavaScriptAppV1Application.class, args);
+	}
+
+}
