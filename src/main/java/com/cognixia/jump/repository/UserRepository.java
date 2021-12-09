58480018cba137c0b317ca410636964f8ea01bfc
zacksafadi <33300646+zacksafadi@users.noreply.github.com>
2021-12-09 12:46:09 -0500

    user service and exception handling

--- src/main/java/com/cognixia/jump/repository/UserRepository.java
+++ src/main/java/com/cognixia/jump/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.cognixia.jump.repository;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cognixia.jump.model.User;
@@ -11,5 +16,15 @@ import com.cognixia.jump.model.User;
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	Optional<User> findByUsername(String username);
+	
+	@Transactional
+	@Modifying
+	@Query("UPDATE User u SET u.username = :username WHERE  u.id = :id")
+	public void updateUsername(@Param(value="username") String username,@Param(value="id") long id);
+	
+	@Transactional
+	@Modifying
+	@Query("UPDATE User u SET u.password = :password WHERE  u.id = :id")
+	public void updatePassword(@Param(value="password") String password,@Param(value="id") long id);
 
 }
